348e269894fd44b18e9aba20888e8c9f1b843d80
German <german@catarino.com.uy>
2017-08-28 16:15:05 -0300

    Getting host zone from aws and updating ip

--- AwsDnsUpdate/Program.cs
+++ AwsDnsUpdate/Program.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Amazon;
+using Amazon.Route53;
+using Amazon.Route53Domains;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using System;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Linq;
+using Amazon.Route53.Model;
+using System.Collections.Generic;
 
 namespace AwsDnsUpdate
 {
@@ -23,45 +31,65 @@ namespace AwsDnsUpdate
             var builder = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json");
-      
+
             Configuration = builder.Build();
 
             #endregion
             try
             {
-                throw new Exception("test exception");
-
                 var ipUrl = Configuration["ipUrl"];
 
-                var client = new HttpClient();
+                var webClient = new HttpClient();
 
-                var stringresponse = await client.GetStringAsync(ipUrl);
+                var stringresponse = await webClient.GetStringAsync(ipUrl);
 
                 var jsonResponse = JsonConvert.DeserializeObject<dynamic>(stringresponse);
 
-                string ip = jsonResponse.ip_addr;
+                string externalIp = jsonResponse.ip_addr;
+
+                var client = new AmazonRoute53Client(Configuration["aws:key"], Configuration["aws:secret"], RegionEndpoint.USEast2);
+
+                var domain = Configuration["aws:domain"];
+                if (!domain.EndsWith("."))
+                {
+                    domain += ".";
+                }
+
+                var zone = (await client.ListHostedZonesAsync()).HostedZones.FirstOrDefault(x => x.Name == domain);
+
+                if (zone == null)
+                    throw new Exception("There's no hosted zone for domain = " + domain);
+
+                var subDomain = Configuration["aws:subdomain"] + "." + domain;
+                var recordSet = (await client.ListResourceRecordSetsAsync(new Amazon.Route53.Model.ListResourceRecordSetsRequest { HostedZoneId = zone.Id }))
+                                .ResourceRecordSets.FirstOrDefault(x => x.Type == "A" && x.Name == subDomain);
+
+                if (recordSet == null)
+                    throw new Exception("There's no subdomain = " + subDomain);
+
+                var resourceRecord = recordSet.ResourceRecords.FirstOrDefault();
+
+                if (resourceRecord == null)
+                    throw new Exception("There's no config value for subdomain = " + subDomain);
+
+                if (resourceRecord.Value != externalIp)
+                {
+                    resourceRecord.Value = externalIp;
+                    var change = new Change()
+                    {
+                        ResourceRecordSet = recordSet,
+                        Action = ChangeAction.UPSERT
+                    };
+
+                    var changeBatch = new ChangeBatch()
+                    {
+                        Changes = new List<Change> { change }
+                    };
 
 
+                    await client.ChangeResourceRecordSetsAsync(new ChangeResourceRecordSetsRequest { HostedZoneId = zone.Id, ChangeBatch = changeBatch });
+                }
 
-                //Console.WriteLine(ip);
-
-                //using (StreamWriter sw = File.AppendText(Path.Combine(Directory.GetCurrentDirectory(), "output.txt")))
-                //{
-                //    sw.WriteLine("ip=" + ip);
-                //}
-
-
-
-
-                //Console.WriteLine($"ipUrl = {Configuration["ipUrl"]}");
-                //Console.WriteLine(
-                //    $"awskey = {Configuration["aws:key"]}");
-                //Console.WriteLine(
-                //    $"awssecret = {Configuration["aws:secret"]}");
-                //Console.WriteLine();
-
-
-                //Console.Read();
             }
             catch (Exception e)
             {
@@ -81,7 +109,7 @@ namespace AwsDnsUpdate
                 Directory.CreateDirectory(basePath);
             }
 
-            return Path.Combine(basePath , logName);
+            return Path.Combine(basePath, logName);
         }
 
 
@@ -90,7 +118,7 @@ namespace AwsDnsUpdate
             var logName = GetFileName();
             using (StreamWriter sw = File.AppendText(Path.Combine(Directory.GetCurrentDirectory(), logName)))
             {
-                sw.WriteLine(DateTime.Now.ToString() + " - " +  message);
+                sw.WriteLine(DateTime.Now.ToString() + " - " + message);
                 sw.WriteLine();
                 sw.WriteLine("_______________________________________________________________________________________________");
             }
