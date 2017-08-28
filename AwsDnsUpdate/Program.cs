3c2fcfd6052573b76252e589d0ecf000d05f80a6
German <german@catarino.com.uy>
2017-08-28 13:05:43 -0300

    Adding logging class

--- AwsDnsUpdate/Program.cs
+++ AwsDnsUpdate/Program.cs
@@ -22,34 +22,85 @@ namespace AwsDnsUpdate
             #region LoadingAppSettings
             var builder = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
-           .AddJsonFile("appsettings.json");
-
+            .AddJsonFile("appsettings.json");
+      
             Configuration = builder.Build();
 
             #endregion
+            try
+            {
+                var ipUrl = Configuration["ipUrl"];
 
-            var ipUrl = Configuration["ipUrl"];
+                var client = new HttpClient();
 
-            var client = new HttpClient();
+                var stringresponse = await client.GetStringAsync(ipUrl);
 
-            var stringresponse = await client.GetStringAsync(ipUrl);
+                var jsonResponse = JsonConvert.DeserializeObject<dynamic>(stringresponse);
 
-            var jsonResponse = JsonConvert.DeserializeObject<dynamic>(stringresponse);
-
-            string ip = jsonResponse.ip_addr;
-
-            Console.WriteLine(ip);
-
-
-            //Console.WriteLine($"ipUrl = {Configuration["ipUrl"]}");
-            //Console.WriteLine(
-            //    $"awskey = {Configuration["aws:key"]}");
-            //Console.WriteLine(
-            //    $"awssecret = {Configuration["aws:secret"]}");
-            //Console.WriteLine();
+                string ip = jsonResponse.ip_addr;
 
 
-            Console.Read();
+
+                //Console.WriteLine(ip);
+
+                //using (StreamWriter sw = File.AppendText(Path.Combine(Directory.GetCurrentDirectory(), "output.txt")))
+                //{
+                //    sw.WriteLine("ip=" + ip);
+                //}
+
+
+
+
+                //Console.WriteLine($"ipUrl = {Configuration["ipUrl"]}");
+                //Console.WriteLine(
+                //    $"awskey = {Configuration["aws:key"]}");
+                //Console.WriteLine(
+                //    $"awssecret = {Configuration["aws:secret"]}");
+                //Console.WriteLine();
+
+
+                //Console.Read();
+            }
+            catch (Exception e)
+            {
+                Logger.Log(e);
+            }
+
+            
+        }
+
+
+
+    }
+
+    public static class Logger
+    {
+        public static string logName = DateTime.Today.ToString("yyyyMMdd") + "_log.txt";
+
+        public static void Log(string message)
+        {
+
+            using (StreamWriter sw = File.AppendText(Path.Combine(Directory.GetCurrentDirectory(), logName)))
+            {
+                sw.WriteLine(DateTime.Now.ToString() + " - " +  message);
+            }
+
+            Console.WriteLine(message);
+        }
+
+        public static void Log(Exception e)
+        {
+
+            using (StreamWriter sw = File.AppendText(Path.Combine(Directory.GetCurrentDirectory(), logName)))
+            {
+                sw.WriteLine(DateTime.Now.ToString() + " - " + e.Message);
+                if (!string.IsNullOrEmpty(e.StackTrace))
+                {
+                    sw.WriteLine("\t" + e.StackTrace);
+                }
+            }
+
+            Console.WriteLine(e);
         }
     }
 }
