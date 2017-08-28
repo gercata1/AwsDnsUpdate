032d2f33dadb7f6a18727a5c60c94fc4a039e64a
German <german@catarino.com.uy>
2017-08-27 23:27:28 -0300

    Adding appsettings and getting ip

--- AwsDnsUpdate/Program.cs
+++ AwsDnsUpdate/Program.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace AwsDnsUpdate
 {
     class Program
     {
+        public static IConfigurationRoot Configuration { get; set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+
+        static async Task MainAsync(string[] args)
+        {
+            #region LoadingAppSettings
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+            #endregion
+
+            var ipUrl = Configuration["ipUrl"];
+
+            var client = new HttpClient();
+
+            var stringresponse = await client.GetStringAsync(ipUrl);
+
+            var jsonResponse = JsonConvert.DeserializeObject<dynamic>(stringresponse);
+
+            string ip = jsonResponse.ip_addr;
+
+            Console.WriteLine(ip);
+
+
+            //Console.WriteLine($"ipUrl = {Configuration["ipUrl"]}");
+            //Console.WriteLine(
+            //    $"awskey = {Configuration["aws:key"]}");
+            //Console.WriteLine(
+            //    $"awssecret = {Configuration["aws:secret"]}");
+            //Console.WriteLine();
+
+
+            Console.Read();
         }
     }
 }
